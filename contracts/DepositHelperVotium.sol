0a6418664da4f8d9f15070ebfcd4de70de12cd04
oo-00 <29607330+oo-00@users.noreply.github.com>
2025-10-19 09:39:07 -0400

    Post Deployment tests for debugging 2nd claim

--- contracts/DepositHelperVotium.sol
+++ contracts/DepositHelperVotium.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import { IERC20, SafeERC20 } from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+import { IERC20, SafeERC20 } from './SafeERC20.sol';
 
 interface Votium {
     function activeRound() external view returns (uint256);
--- contracts/DepositPlatformDivider.sol
+++ contracts/DepositPlatformDivider.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-import { IERC20, SafeERC20 } from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+import { IERC20, SafeERC20 } from './SafeERC20.sol';
 import { IDepositHelper } from './IDepositHelper.sol';
 
 interface VestingContract {
